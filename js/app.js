9f9312a45f7f1c0dcb741bfb764f49d66e550690
Ahmed Mahmoud <ahmed.mahmoud8116@gmail.com>
2020-09-23 01:46:19 +0200

    two functions done

--- js/app.js
+++ js/app.js
@@ -17,15 +17,22 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.querySelectorAll('section'); //to get all of the sections in the document
+const navList = document.getElementById('navbar__list'); //to get the list of navbar by id
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-function buildNavBar(){
-	
+function navElementsHelper(section){
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.textContent = section.getAttribute('data-nav');
+    a.className = 'menu__link';
+    a.setAttribute('href', `#${section.getAttribute('id')}`);
+    li.appendChild(a);
+    navList.appendChild(li);
 }
 
 
@@ -36,13 +43,25 @@ function buildNavBar(){
 */
 
 // build the nav
-
+function buildNavBar(){
+	sections.forEach(section => {
+        navElementsHelper(section);
+    })
+}
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToAnchor(event){
+    if(event.target.nodeName === 'a'){
+        const dest = event.target.getAttribute('href');
+        const section = document.querySelector(dest);
+        const x = section.getBoundingClientRect().x; //returns the size of an element and its position relative to the viewport
+        const y = section.getBoundingClientRect().y;
+        window.scrollTo(x, y);
+    } 
+}
 
 /**
  * End Main Functions
@@ -50,9 +69,11 @@ function buildNavBar(){
  * 
 */
 
-// Build menu 
+// Build menu
+document.addEventListener('DOMContentLoaded', buildNavBar()); 
 
 // Scroll to section on link click
+navList.addEventListener('click', scrollToAnchor);
 
 // Set sections as active
 
